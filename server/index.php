a565fa14e65deac111ae4c5fd548bd9840bae5d1
x29a <0.x29a.0@gmail.com>
2020-10-09 00:29:44 +0200

    use sqlite3 db instead of textfile - save values to textfile for now though
    using sqlite3 allows to filter by sensor id via URL

--- server/index.php
+++ server/index.php
@@ -1,13 +1,31 @@
 <?php
 $filename = 'data.log';
+$filenameDB = 'data.db';
+
+if(!file_exists($filenameDB)) {file_put_contents($filenameDB, '');}
+
+$db = new SQLite3($filenameDB, SQLITE3_OPEN_READWRITE);
+$db->enableExceptions(true);
+
+$db->query('CREATE TABLE IF NOT EXISTS sensor_values (timestamp TEXT, sensorIP TEXT, sensorID TEXT, data TEXT);');
 
 if(isset($_POST) && !empty($_POST))
 {
 	$dataSet = array();
 	$dataSet['datetime'] = date('YmdHis');
 	$dataSet = array_merge($dataSet, $_POST);
+
 	$data = json_encode($dataSet);
 
+	$sensorID = '';
+	if(isset($dataSet['id']) && !empty($dataSet['id']))
+	{
+		$sensorID = SQLite3::escapeString($dataSet['id']);
+	}
+
+	$db->query("INSERT INTO sensor_values (timestamp, sensorIP, sensorID, data) VALUES ('".$dataSet['datetime']."', '".$_SERVER['REMOTE_ADDR']."', '".$sensorID."', '".SQLite3::escapeString($data)."');");
+
+	// save to log for now just to be save
 	file_put_contents($filename, $data."\r\n".file_get_contents($filename));
 	echo "ok";
 }
@@ -15,8 +33,29 @@ else if(isset($_GET['show']))
 {
 	$num = 20;
 	if(isset($_GET['num'])) { $num = (int)$_GET['num']; }
-	$file_data = array_slice(file($filename), 0, $num);
-	
+	if(isset($_GET['id'])) { $id = SQLite3::escapeString($_GET['id']); }
+
+	$queryString = 'SELECT data FROM sensor_values';
+
+	if(isset($id) && !empty($id))
+	{
+		$queryString .= ' WHERE sensorID = "'.$id.'"';
+	}
+	$queryString .= ' ORDER BY timestamp DESC LIMIT '.$num;
+
+	$result = $db->query($queryString);
+
+	$db_data = array();
+	if($result->numColumns() > 0)
+	{
+		while ($res= $result->fetchArray(SQLITE3_ASSOC))
+		{
+			array_push($db_data, $res['data']);
+		}
+	}
+	$result->finalize();
+	$plotData = array_reverse($db_data);
+
 	// parse file data and extract interesting information
 	$temp = array();
 	$relHum = array();
@@ -27,7 +66,7 @@ else if(isset($_GET['show']))
 	$chart1Max = 1;
 	$chart2Min = 0;
 	$chart2Max = 1;
-	foreach($file_data as $arrayKey => $jsonValue) 
+	foreach($plotData as $arrayKey => $jsonValue)
 	{
 		$newValueArray = json_decode($jsonValue, true);
 		
